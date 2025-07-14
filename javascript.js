193cce12afd2a0087dc3e31618602b296914b8fe
Olivia Wojciechowska <oliwoj209@gmail.com>
2025-07-13 18:24:10 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+        // Hamburger menu toggle
+        function toggleMenu() {
+            const navLinks = document.querySelector('.nav-links');
+            navLinks.classList.toggle('active');
+        }
